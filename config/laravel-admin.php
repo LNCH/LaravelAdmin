66933c0fdc4cda8ae02b4136a253d6d7f6091e17
Tom Lynch <tom@lnch.co.uk>
2018-11-21 15:19:49 +0000

    Add bootstrap to config and base layout, with core CSS

--- config/laravel-admin.php
+++ config/laravel-admin.php
@@ -3,4 +3,5 @@
 return [
     'default_body_id' => 'default_body_id',
     'default_body_classes' => [],
+    'bootstrap_url' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
 ];
--- src/views/components/base.blade.php
+++ src/views/components/base.blade.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @stack("meta")
         <title>@yield("site_title", "Laravel Admin")</title>
+        <link rel="stylesheet" href="{{ config("laravel-admin.bootstrap_url", "") }}" />
+        <link rel="stylesheet" href="{{ asset("vendor/lnch/laravel-admin/css/laravel-admin.core.css") }}" />
         @stack("head_scripts")
         @stack("head_end")
     </head>
