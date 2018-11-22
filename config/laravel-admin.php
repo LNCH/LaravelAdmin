f39476f3878a0c728782bc9647832ec711c31d39
Tom Lynch <tom@lnch.co.uk>
2018-11-22 15:24:51 +0000

    Add more config options

--- config/laravel-admin.php
+++ config/laravel-admin.php
@@ -2,5 +2,20 @@
 
 return [
     'default_body_classes' => [],
-    'bootstrap_url' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
+    'bootstrap_url' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+
+    'account_dropdown_menu' => [
+        [
+            'url' => '/user/profile',
+            'label' => 'Profile'
+        ],
+        [
+            'url' => '/user/preferences',
+            'label' => 'Preferences',
+        ],
+        [
+            'url' => '/logout',
+            'label' => 'Logout',
+        ]
+    ]
 ];
