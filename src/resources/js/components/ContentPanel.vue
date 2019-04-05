341290a294069eca7535f6cc6cfe598a80e29763
Tom Lynch <tom@lnch.co.uk>
2019-04-05 12:35:12 +0100

    Move CSS variables out of the ContentPanel Vue file

--- src/resources/js/components/ContentPanel.vue
+++ src/resources/js/components/ContentPanel.vue
@@ -62,46 +62,36 @@
 <style lang="scss">
     @import "../../sass/utilities";
 
-    :root {
-        --panel-heading-font-size: 0.8rem;
-        --panel-heading-line-height: 1.5;
-        --panel-heading-x-padding: 1.5rem;
-        --panel-heading-y-padding: 0.75rem;
-
-        --panel-body-x-padding: 1.5rem;
-        --panel-body-y-padding: 1rem;
-    }
-
     ._laravel-admin .content-panel {
-        background: white;
-        font-size: 1rem;
-        box-shadow: 0px 0 8px 1px var(--layout-border-color);
+        background: var(--background);
+        font-size: var(--font-size);
+        box-shadow: var(--box-shadow);
 
         .body {
-            padding: 0 var(--panel-body-x-padding);
+            padding: 0 var(--body-x-padding);
             max-height: 0px;
             overflow: hidden;
-            transition: 0.3s ease all;
+            transition: var(--body-transition);
 
             &.is-open {
-                padding: var(--panel-body-y-padding) var(--panel-body-x-padding);
+                padding: var(--body-y-padding) var(--body-x-padding);
                 max-height: none;
                 overflow: auto;
             }
 
             .full-width {
-                margin-left: calc(var(--panel-body-x-padding) * -1);
-                width: calc(100% + (var(--panel-body-x-padding) * 2));
+                margin-left: calc(var(--body-x-padding) * -1);
+                width: calc(100% + (var(--body-x-padding) * 2));
                 border-left: none;
                 border-right: none;
 
                 tr {
                    td:first-child, th:first-child {
-                       padding-left: var(--panel-body-x-padding);
+                       padding-left: var(--body-x-padding);
                        border-left: none;
                    }
                    td:last-child, th:last-child {
-                       padding-right: var(--panel-body-x-padding);
+                       padding-right: var(--body-x-padding);
                        border-right: none;
                    }
                 }
@@ -110,36 +100,36 @@
 
         .header {
             display: flex;
-            border-bottom: 1px solid var(--layout-border-color);
+            border-bottom: var(--header-bottom-border);
             @include no-select;
 
             .title {
                 flex: 1 1 auto;
-                padding: var(--panel-heading-y-padding) var(--panel-heading-x-padding);
-                font-size: var(--panel-heading-font-size);
-                line-height: var(--panel-heading-line-height);
-                text-transform: uppercase;
-                font-weight: bold;
-                letter-spacing: 0.5px;
+                padding: var(--heading-y-padding) var(--heading-x-padding);
+                font-size: var(--heading-font-size);
+                line-height: var(--heading-line-height);
+                text-transform: var(--heading-text-transform);
+                font-weight: var(--heading-font-weight);
+                letter-spacing: var(--heading-letter-spacing);
             }
         }
 
         button.action {
             height: 100%;
-            width: calc((var(--panel-heading-font-size) * var(--panel-heading-line-height)) + (var(--panel-heading-y-padding) * 2));
+            width: calc((var(--heading-font-size) * var(--heading-line-height)) + (var(--heading-y-padding) * 2));
             @include clearAppearance;
-            font-size: 0.6rem;
-            font-weight: normal;
-            border-left: 1px solid var(--layout-border-color);
+            font-size: var(--action-button-font-size);
+            font-weight: var(--action-button-font-weight);
+            border-left: var(--action-button-divider-border);
             cursor: pointer;
-            transition: 0.3s ease all;
+            transition: var(--action-button-transition);
 
             &:focus {
                 outline: none;
             }
 
             &:hover {
-                background: rgba(0, 0, 0, 0.05);
+                background: var(--action-button-hover-background);
             }
         }
     }
