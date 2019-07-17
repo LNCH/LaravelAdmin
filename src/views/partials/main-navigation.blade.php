25ce6260eaa701da3fe0202dc8a422c8be0f46ac
Tom Lynch <tom@lnch.co.uk>
2019-07-17 09:31:31 +0100

    Check for null nav items

--- src/views/partials/main-navigation.blade.php
+++ src/views/partials/main-navigation.blade.php
@@ -5,12 +5,14 @@
 
     <ul>
     @foreach(config("laravel-admin.main_navigation") as $item)
-        @if(isset($item['title']))
-            <li class="title">
-                {{ $item['title'] }}
-            </li>
-        @else
-            @include("laravel-admin::partials.nav-item", ['item' => $item])
+        @if($item !== null)
+            @if(isset($item['title']))
+                <li class="title">
+                    {{ $item['title'] }}
+                </li>
+            @else
+                @include("laravel-admin::partials.nav-item", ['item' => $item])
+            @endif
         @endif
     @endforeach
     </ul>
